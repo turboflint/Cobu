d27ba4eb2680f2ebaf4f9c0a81c641d0f7011da8
OverRay <108870114+electronic1028@users.noreply.github.com>
2023-08-24 10:56:26 +0800

    Add files via upload

--- quicker.js
+++ quicker.js
@@ -1,4 +1,6 @@
-(function color() {
+console.warn('v1.0.2 quicker.js is a test version.');
+console.error('can not fetch the latest version,because service is locked.');
+        (function color() {
     var elements = document.querySelectorAll('[color]');
     elements.forEach(function(element) {
         var color = element.getAttribute('color');
@@ -45,5 +47,27 @@
     elements.forEach(function(element) {
         var font = element.getAttribute('font');
         element.style.fontSize = font;
+    });
+})();
+
+/* v1.0.1 */
+
+(function hover() {
+    var elements = document.querySelectorAll('[hover]');
+    elements.forEach(function(element) {
+        var hover = element.getAttribute('hover');
+        element.style.background = 'white';
+        element.style.color = hover;
+        element.style.border = '1px solid ' + hover;
+        element.addEventListener('mouseover', function() {
+            element.style.background = hover;
+            element.style.color = 'white';
+            elememt.style.border = '1px solid ' + hover;
+        });
+        element.addEventListener('mouseout', function() {
+            element.style.background = 'white';
+            element.style.color = hover;
+            element.style.border = '1px solid ' + hover;
+        });
     });
 })();
